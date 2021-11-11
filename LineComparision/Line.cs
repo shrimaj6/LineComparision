d59759fffaa48615bd1cadfd5706b5a7794e6c96
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-11 16:03:47 +0530

    added line comparision program

--- LineComparision/Line.cs
+++ LineComparision/Line.cs
@@ -6,7 +6,7 @@ namespace LineComparision
 {
     public class Line
     {
-        public void computelength(int x1, int y1, int x2, int y2)
+        public double computelength(int x1, int y1, int x2, int y2)
         {
 
             int xabs = Math.Abs(x2 - x1);
@@ -15,7 +15,8 @@ namespace LineComparision
             int distance_square = (int)(Math.Pow(xabs, 2) + Math.Pow(yabs, 2));
 
             double length = Math.Sqrt(distance_square);
-            Console.WriteLine("The length between " + x1 + "," + y1 + " and " + x2 + "," + y2 + " is: " + Math.Round(length, 4));
+            return length;
+
         }
     }
 }
--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -21,8 +21,40 @@ namespace LineComparision
 
             Console.WriteLine("Enter y2: ");
             int y2 = int.Parse(Console.ReadLine());
+
             Line obj1 = new Line();
-            obj1.computelength(x1, y1, x2, y2);
+            double length1 = obj1.computelength(x1, y1, x2, y2);
+            //for line 2
+            Console.WriteLine("Enter x1: ");
+            int x11 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter y1: ");
+            int y11 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter x2: ");
+            int x22 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter y2: ");
+            int y22 = int.Parse(Console.ReadLine());
+
+
+
+            Line obj2 = new Line();
+            double length2 = obj1.computelength(x11, y11, x22, y22);
+
+            length1 = Math.Round(length1, 3);
+            length2 = Math.Round(length2, 3);
+
+            if (length1 == length2)
+            {
+                Console.WriteLine("Two lines are equal " + length1 + " " + length2);
+            }
+            else
+            {
+                Console.WriteLine("Two lines are not equal " + length1 + " " + length2);
+            }
+
         }
 
 
