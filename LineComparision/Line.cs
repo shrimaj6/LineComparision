0c38382ff7dfd5cebfde12f2bf8b8609fcca5b3e
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-11 15:50:10 +0530

    this is length of two lines.

---
+++ LineComparision/Line.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparision
+{
+    public class Line
+    {
+        public void computelength(int x1, int y1, int x2, int y2)
+        {
+
+            int xabs = Math.Abs(x2 - x1);
+            int yabs = Math.Abs(y2 - y1);
+
+            int distance_square = (int)(Math.Pow(xabs, 2) + Math.Pow(yabs, 2));
+
+            double length = Math.Sqrt(distance_square);
+            Console.WriteLine("The length between " + x1 + "," + y1 + " and " + x2 + "," + y2 + " is: " + Math.Round(length, 4));
+        }
+    }
+}
--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -7,6 +7,24 @@ namespace LineComparision
         static void Main(string[] args)
         {
             Console.WriteLine("Hello welcome to line comparision programs");
+
+
+
+            Console.WriteLine("Enter x1: ");
+            int x1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter y1: ");
+            int y1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter x2: ");
+            int x2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter y2: ");
+            int y2 = int.Parse(Console.ReadLine());
+            Line obj1 = new Line();
+            obj1.computelength(x1, y1, x2, y2);
         }
+
+
     }
 }
