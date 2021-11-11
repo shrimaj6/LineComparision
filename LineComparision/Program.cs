f414836c61cada66c02b2b75eaaab618e72d1022
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-11 15:43:20 +0530

    Add project files.

---
+++ LineComparision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to line comparision programs");
+        }
+    }
+}
